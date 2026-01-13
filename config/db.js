ccecca8fc5a502fd4faf79e5b0fff0b3c205b2e1
jeel hapani <133969174+jeelhapani@users.noreply.github.com>
2026-01-13 09:36:52 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -11,6 +11,27 @@ function mongoConnection() {
     });
 }
 
-
-
 module.exports = mongoConnection();
+
+
+
+// const mysql = require("mysql2");
+
+// let connection = mysql.createPool({
+//     host: "127.0.0.1",
+//     port: 3306,
+//     user: "root",
+//     password: "1492000",
+//     database: "test"
+// });
+
+// const mysqlQuery = (qry) => {
+//     return new Promise((resolve, reject) => {
+//         connection.query(qry, (err, row) => {
+//             if (err) return reject(err);
+//             resolve(row);
+//         });
+//     });
+// };
+
+// module.exports = { connection, mysqlQuery };
